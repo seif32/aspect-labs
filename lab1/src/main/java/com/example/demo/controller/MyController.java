21fe1552582d0193694f41c4550a4fb2a68b919f
Seif Mohamed <20p1710@eng.asu.edu.eg>
2025-03-04 16:15:31 +0200

    Add DELETE method

--- lab1/src/main/java/com/example/demo/controller/MyController.java
+++ lab1/src/main/java/com/example/demo/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,5 +25,10 @@ public class MyController {
   @PutMapping
   public ResponseEntity<String> updateResource() {
     return ResponseEntity.ok("PUT Request: Updating resource");
+  }
+
+  @DeleteMapping
+  public ResponseEntity<String> deleteResource() {
+    return ResponseEntity.ok("DELETE Request: Deleting resource");
   }
 }
