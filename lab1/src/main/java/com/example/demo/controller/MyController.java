bee7f97a0eafd20151810c5406c09509bd95ee86
Seif Mohamed <20p1710@eng.asu.edu.eg>
2025-03-04 16:14:46 +0200

    Add POST method

--- lab1/src/main/java/com/example/demo/controller/MyController.java
+++ lab1/src/main/java/com/example/demo/controller/MyController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class MyController {
   @GetMapping
   public ResponseEntity<String> getResource() {
     return ResponseEntity.ok("GET Request: Fetching resource");
+  }
+
+  @PostMapping
+  public ResponseEntity<String> createResource() {
+    return ResponseEntity.ok("POST Request: Creating resource");
   }
 }
