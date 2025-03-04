19baf213fcc1e288f3aa4641cadb772aefc15031
Seif Mohamed <20p1710@eng.asu.edu.eg>
2025-03-04 16:13:34 +0200

    Add GET method

---
+++ lab1/src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/resource")
+public class MyController {
+
+  @GetMapping
+  public ResponseEntity<String> getResource() {
+    return ResponseEntity.ok("GET Request: Fetching resource");
+  }
+}
