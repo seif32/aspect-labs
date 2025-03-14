69972a25bc073d1b44896ee272a104c2a38097ac
Seif Mohamed <20p1710@eng.asu.edu.eg>
2025-03-14 18:32:41 +0200

    Fix error in service

--- lab2/src/main/java/com/example/services/UserService.java
+++ lab2/src/main/java/com/example/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.lab2.models.User;
 import com.example.lab2.repository.UserRepository;
 
+@Service
 public class UserService {
   @Autowired
   private UserRepository userRepository;
