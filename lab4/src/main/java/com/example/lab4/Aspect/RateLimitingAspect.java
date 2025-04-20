7d0efeab9cf700bedfc982d1f046f23e802c094c
Seif Mohamed <20p1710@eng.asu.edu.eg>
2025-04-20 22:05:39 +0200

    Fix bug in rate-limit

--- lab4/src/main/java/com/example/lab4/Aspect/RateLimitingAspect.java
+++ lab4/src/main/java/com/example/lab4/Aspect/RateLimitingAspect.java
@@ -16,19 +16,27 @@ public class RateLimitingAspect {
   @Autowired
   private RedisTemplate<String, String> redisTemplate;
 
-  @Around("@annotation(com.example.redislab.annotation.RateLimited)")
+  @Around("@annotation(com.example.lab4.Annotation.RateLimited)")
   public Object rateLimit(ProceedingJoinPoint joinPoint) throws Throwable {
     String key = "rate_limit:" + joinPoint.getSignature().toShortString();
+    System.out.println("=== RATE LIMIT CHECK ===");
+    System.out.println("Using key: " + key);
+
     Long currentCount = redisTemplate.opsForValue().increment(key, 1);
+    System.out.println("Current count: " + currentCount);
 
     if (currentCount == 1) {
-      redisTemplate.expire(key, 1, TimeUnit.MINUTES);
+      System.out.println("Setting expiration...");
+      Boolean expirySet = redisTemplate.expire(key, 1, TimeUnit.MINUTES);
+      System.out.println("Expiry set: " + expirySet);
     }
 
-    if (currentCount > 10) {
+    if (currentCount > 3) {
+      System.out.println("=== RATE LIMIT EXCEEDED ===");
       throw new RuntimeException("Rate limit exceeded - try again later");
     }
 
+    System.out.println("=== REQUEST ALLOWED ===");
     return joinPoint.proceed();
   }
 }
--- lab4/src/main/java/com/example/lab4/Model/Product.java
+++ lab4/src/main/java/com/example/lab4/Model/Product.java
@@ -1,5 +1,7 @@
 package com.example.lab4.Model;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +10,9 @@ import lombok.Data;
 
 @Entity
 @Data
-public class Product {
+public class Product implements Serializable {
+  private static final long serialVersionUID = 1L;
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
