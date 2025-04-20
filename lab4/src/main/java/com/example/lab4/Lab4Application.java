6d8a98250da2f3a8bf879fc6321f7d2ccb43ebb4
Seif Mohamed <20p1710@eng.asu.edu.eg>
2025-04-20 21:27:53 +0200

    Enable caching

--- lab4/src/main/java/com/example/lab4/Lab4Application.java
+++ lab4/src/main/java/com/example/lab4/Lab4Application.java
@@ -2,12 +2,14 @@ package com.example.lab4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Lab4Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lab4Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Lab4Application.class, args);
+  }
 
 }
